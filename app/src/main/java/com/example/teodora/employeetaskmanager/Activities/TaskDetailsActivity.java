825023d363f30959cd599b53305a33dd965f076d
ViktorGagaleski <v.gagaleski@yahoo.com>
2017-01-18 01:18:31 +0100

    XML for task details in privacy policy

--- app/src/main/java/com/example/teodora/employeetaskmanager/Activities/TaskDetailsActivity.java
+++ app/src/main/java/com/example/teodora/employeetaskmanager/Activities/TaskDetailsActivity.java
@@ -6,16 +6,18 @@ import android.support.v7.widget.Toolbar;
 
 import com.example.teodora.employeetaskmanager.R;
 
-public class PrivacyPolicyActivity extends AppCompatActivity {
+public class TaskDetailsActivity extends AppCompatActivity {
+
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_details);
+
         // Adding Toolbar to AssignTask screen
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Privacy Policy");
+        toolbar.setTitle("Assign task");
         setSupportActionBar(toolbar);
     }
 }
